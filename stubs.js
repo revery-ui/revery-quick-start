83740b6b03fffb9560b82fa63440008c9abe8c61
Bryan Phelps <bryphe@outrunlabs.com>
2019-10-15 17:31:32 -0500

    Add instructions / dependencies for JS builds (#45)
    * Add dependencies for JS build
    
    * Update README
    
    * Add CI steps
    
    * Fix title
    
    * Add js lockfile
    
    * Add missing deps for linux builds

---
+++ stubs.js
@@ -0,0 +1,15 @@
+// ** OCaml Runtime stubs **
+// js_of_ocaml does not provide stubs for these methods by default
+// we'll just put them to no-ops
+
+// Provides: caml_thread_self
+function caml_thread_self() { }
+
+// Provides: caml_thread_new
+function caml_thread_new() { }
+
+// Provides: caml_mutex_lock
+function caml_mutex_lock() { }
+
+// Provides: caml_mutex_unlock
+function caml_mutex_unlock() { }
